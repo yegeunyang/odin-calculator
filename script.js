95dc595e5aec693b8d3df7ee0f0ac794e2c2ecd4
yegeunyang <yegeunyang@icloud.com>
2025-01-08 15:12:35 -0600

    Add eventlisteners to the buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let operand1 = 0;
-let operand2 = 0;
-let operator = '';
+let operand1 = "";
+let operand2 = "";
+let operator = "";
 
 function operate(operator, operand1, operand2) {
     function add(operand1, operand2) {
@@ -18,7 +18,7 @@ function operate(operator, operand1, operand2) {
     function divide(operand1, operand2) {
         return operand1 / operand2;
     }
-    
+
     switch (operator) {
         case '+':
             return add(operand1, operand2);
@@ -27,6 +27,65 @@ function operate(operator, operand1, operand2) {
         case '*':
             return multiply(operand1, operand2);
         case '/':
+            if (operand2 === 0) {
+                alert("You can't divide by zero!");
+                return "";
+            }
             return divide(operand1, operand2);
     }
 }
+
+let display = document.querySelector('.display');
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains('number')) {
+            let number = button.textContent;
+            if (operator === '') {
+                if (operand1 === '0') {
+                    operand1 = number;
+                } else {
+                    operand1 += number;
+                }
+                display.textContent = operand1;
+            } else {
+                if (operand2 === '') {
+                    operand2 = number;
+                } else {
+                    operand2 += number;
+                }
+                display.textContent = operand2;
+            }
+        } else if (button.classList.contains('operator')) {
+            switch (button.textContent) {
+                case '+':
+                    operator = '+';
+                    break;
+                case '-':
+                    operator = '-';
+                    break;
+                case '*':
+                    operator = '*';
+                    break;
+                case '/':
+                    operator = '/';
+                    break;
+            }
+        } else if (button.classList.contains('equal')) {
+            let result = operate(operator, parseInt(operand1), parseInt(operand2));
+            operand1 = "" + result;
+            operand2 = "";
+            operator = "";
+            display.textContent = operand1;
+
+            if (result === "") {
+                display.textContent = "0";
+            }
+        } else if (button.classList.contains('clear')) {
+            operand1 = "";
+            operand2 = "";
+            operator = '';
+            display.textContent = "0";
+        }
+    });
+});
